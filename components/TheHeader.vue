0aa48fe28b45f5e47ec06d209b1b3f114cc3e7f6
Andrei Vintila <vintila.andrei@gmail.com>
2024-02-21 17:01:25 +0200

    Added Layout and Header component.

---
+++ components/TheHeader.vue
@@ -0,0 +1,26 @@
+<script lang="ts" setup>
+const colorMode = useColorMode()
+
+function toggleColorMode() {
+  colorMode.preference = colorMode.preference === 'dark' ? 'light' : 'dark'
+}
+
+const links = [
+  [
+    {
+      label: 'Sesizari',
+      to: '/',
+    },
+  ],
+  [
+    {
+      icon: colorMode.value === 'dark' ? 'i-heroicons-sun-20-solid' : 'i-heroicons-moon-20-solid',
+      click: toggleColorMode,
+    },
+  ],
+]
+</script>
+
+<template>
+  <UHorizontalNavigation :links="links" />
+</template>
--- layouts/fullscreen.vue
+++ layouts/fullscreen.vue
@@ -4,7 +4,6 @@
 
 <template>
   <div>
-    <TheHeader />
     <slot />
   </div>
 </template>
